b6f620916c28acb44920069185ad74d223f5149c
Liuxin0 <liuxin@imudges.com>
2017-07-29 10:13:02 +0800

    UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,22 +19,32 @@ public class Main extends Application {
 
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setPadding(new Insets(20,20,20,20));
-        Scene scene = new Scene(gridPane,600,550);
+        gridPane.setMinHeight(420);
+        gridPane.setMinWidth(560);
+        gridPane.setHgap(22);
+        gridPane.setVgap(22);
+        gridPane.setPadding(new Insets(10,20,10,20));
+        Scene scene = new Scene(gridPane,800,600);
 
+        primaryStage.setMinWidth(560);
+        primaryStage.setMinHeight(420);
         primaryStage.setScene(scene);
         primaryStage.show();
 
         TextArea inputTextField = new TextArea();
-        gridPane.add(inputTextField,0,1,1,3);
+
+        Label inputLabel = new Label("从此输入");
+        inputLabel.setFont(Font.font(20));
+        gridPane.add(inputLabel,0,0,3,1);
+        gridPane.add(inputTextField,0,1,3,16);
 
         TextArea outputTextField = new TextArea();
-        gridPane.add(outputTextField,6,1,1,3);
+        outputTextField.setEditable(false);
+        outputTextField.setWrapText(true);
+        gridPane.add(outputTextField,5,1,3,16);
 
         Button button = new Button("转换");
-        gridPane.add(button,1,3,5,1);
+        gridPane.add(button,3,8,2,1);
 
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
